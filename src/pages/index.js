0a10f3be821bc4a33ad37af186e2e96d13d9bcc0
john-smilga <johnsmilga@gmail.com>
2020-03-23 15:53:30 -0700

    8-inline-css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+// global css
+// multiple elements
 import React from "react"
 import Layout from "../components/layout"
 export default () => (
   <Layout>
-    <h1>hello from gatsby</h1>
+    <h1 style={{ color: "red", textTransform: "uppercase" }}>
+      hello from gatsby
+    </h1>
   </Layout>
 )
