e1a328430dd7488012c5b01ae8c811032318bb5b
john-smilga <johnsmilga@gmail.com>
2020-03-23 11:00:18 -0700

    3-pages

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const blog = () => {
+  return (
+    <div>
+      <h1>this is our blog page</h1>
+    </div>
+  )
+}
+
+export default blog
---
+++ src/pages/products.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class products extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is our products</h1>
+      </div>
+    )
+  }
+}
