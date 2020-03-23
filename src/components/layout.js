8ce0fb200ef69a33fd6abc73b926c03ba7d86ca6
john-smilga <johnsmilga@gmail.com>
2020-03-23 16:25:45 -0700

    9-global-css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import "./layout.css"
 const layout = ({ children }) => {
   return (
     <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 // global css
-// multiple elements
 import React from "react"
 import Layout from "../components/layout"
 export default () => (
   <Layout>
-    <h1 style={{ color: "red", textTransform: "uppercase" }}>
+    <h1 style={{ color: "green", textTransform: "uppercase" }}>
       hello from gatsby
     </h1>
+    <h1>hello world</h1>
+    <h1>hello people</h1>
   </Layout>
 )
