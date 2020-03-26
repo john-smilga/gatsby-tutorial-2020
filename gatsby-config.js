03b75d160204d3abb6bc4366708e290c06fb1505
john-smilga <johnsmilga@gmail.com>
2020-03-26 10:32:28 -0700

    16-siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Gatsby Tutorial",
+    description: "some random description",
+    author: "@johndoe",
+    data: ["item 1", "item 2"],
+    person: { name: "peter", age: 32 },
+  },
+
   plugins: [`gatsby-plugin-styled-components`],
   /* Your site config here */
 }
