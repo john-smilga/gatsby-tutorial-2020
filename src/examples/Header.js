01649727ea6e022e0f8a565f20e886d929fabe64
john-smilga <johnsmilga@gmail.com>
2020-03-26 17:36:05 -0700

    22-manual-useStaticQuery

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,29 +1,39 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          person {
-            age
-            name
-          }
-          author
-          data
-          description
-          title
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        person {
+          age
+          name
         }
+        author
+        data
+        description
+        title
       }
     }
-  `)
+  }
+`
+const Header = () => {
+  const {
+    site: {
+      siteMetadata: {
+        title,
+        person: { name },
+      },
+    },
+  } = useStaticQuery(getData)
+
   return (
     <div>
-      <h2>{data.site.siteMetadata.person.name}</h2>
-      <h2>{data.site.siteMetadata.person.age}</h2>
+      {/* <h1>title : {data.site.siteMetadata.title}</h1> */}
+      {/* <h1>name : {data.site.siteMetadata.person.name} </h1> */}
+      <h1>title : {title}</h1>
+      <h1>name : {name}</h1>
     </div>
   )
 }
 
-export default ComponentName
+export default Header
--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Header from "../examples/Header"
+import Layout from "../components/layout"
 const examples = () => {
   return (
-    <div>
+    <Layout>
       <h1>hello from examples page</h1>
       <Header />
-    </div>
+    </Layout>
   )
 }
 
