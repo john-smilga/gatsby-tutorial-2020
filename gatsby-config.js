92c399ea6850d98519f7f8fabe9bf2c3dd0e1359
john-smilga <johnsmilga@gmail.com>
2020-03-27 16:36:03 -0700

    33-gatsby-image-plugin-install

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,8 @@ module.exports = {
 
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
