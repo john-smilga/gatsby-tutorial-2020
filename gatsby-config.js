779d377ce1c5832f22e665f931af3af2e1ac3a4f
john-smilga <johnsmilga@gmail.com>
2020-03-27 16:17:07 -0700

    32-sourceInstanceName

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,13 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
   ],
   /* Your site config here */
 }
