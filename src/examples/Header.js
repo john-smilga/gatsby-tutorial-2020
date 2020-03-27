8094ab53dd093c7522b344c26a5f9847dfd191ae
john-smilga <johnsmilga@gmail.com>
2020-03-26 18:12:38 -0700

    24-query-names-fixed

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 const getData = graphql`
-  query FirstQuery () {
+  query FirstQuery {
     site {
       info: siteMetadata {
         person {
