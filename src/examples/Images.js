6a7767d227139482e43c6afe5c1bad06f1d2b420
john-smilga <johnsmilga@gmail.com>
2020-03-28 11:06:49 -0700

    37-images-example-complete

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -7,15 +7,15 @@ const getImages = graphql`
   {
     fixed: file(relativePath: { eq: "image-3.jpeg" }) {
       childImageSharp {
-        fixed(width: 300, height: 400) {
-          src
+        fixed(width: 200, grayscale: true) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
     fluid: file(relativePath: { eq: "image-4.jpeg" }) {
       childImageSharp {
         fluid {
-          src
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
@@ -23,16 +23,25 @@ const getImages = graphql`
 `
 
 const Images = () => {
+  const data = useStaticQuery(getImages)
+  console.log(data)
+
   return (
     <section className="images">
       <article className="single-image">
         <h3>basic image</h3>
+        <img src={img} width="100%" />
       </article>
       <article className="single-image">
         <h3>fixed image/blur</h3>
+        <Image fixed={data.fixed.childImageSharp.fixed} />
       </article>
       <article className="single-image">
         <h3>fluid image/svg</h3>
+        <Image fluid={data.fluid.childImageSharp.fluid} />
+        <div className="small">
+          <Image fluid={data.fluid.childImageSharp.fluid} />
+        </div>
       </article>
     </section>
   )
