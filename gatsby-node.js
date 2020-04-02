f7d88729e526ffa4ae095199e70aad598bd254a3
john-smilga <johnsmilga@gmail.com>
2020-04-02 11:25:50 -0700

    54-gatsby-node

---
+++ gatsby-node.js
@@ -0,0 +1,24 @@
+const path = require("path")
+
+// create pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query GetProducts {
+      products: allContentfulProduct {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
+  result.data.products.nodes.forEach(product => {
+    createPage({
+      path: `/products/${product.slug}`,
+      component: path.resolve(`src/templates/product-template.js`),
+      context: {
+        slug: product.slug,
+      },
+    })
+  })
+}
--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,6 +1,8 @@
 import React from "react"
 
-const productTemplate = () => {
+const productTemplate = props => {
+  console.log(props)
+
   return (
     <div>
       <h1>hello from product template</h1>
