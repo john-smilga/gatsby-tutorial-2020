36257896bfddb3e11b1df1903703d7189b1e40bb
john-smilga <johnsmilga@gmail.com>
2020-04-02 16:27:56 -0700

    58-product-template-complete

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,13 +1,54 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/layout"
+import Image from "gatsby-image"
 
-const productTemplate = props => {
-  console.log(props)
-
+const ComponentName = ({
+  data: {
+    product: {
+      price,
+      title,
+      image: { fixed },
+      info: { info },
+    },
+  },
+}) => {
   return (
-    <div>
-      <h1>hello from product template</h1>
-    </div>
+    <Layout>
+      <div style={{ textAlign: "center" }}>
+        <Link to="/products">back to products</Link>
+        <h1>single product : {title}</h1>
+      </div>
+      <section className="single-product">
+        <article>
+          <Image fixed={fixed} alt={title} />
+        </article>
+        <article>
+          <h1>{title}</h1>
+          <h3>${price}</h3>
+          <p>{info}</p>
+          <button>add to cart</button>
+        </article>
+      </section>
+    </Layout>
   )
 }
 
-export default productTemplate
+export const query = graphql`
+  query GetSingleProduct($slug: String) {
+    product: contentfulProduct(slug: { eq: $slug }) {
+      title
+      price
+      image {
+        fixed(width: 300) {
+          ...GatsbyContentfulFixed
+        }
+      }
+      info {
+        info
+      }
+    }
+  }
+`
+
+export default ComponentName
