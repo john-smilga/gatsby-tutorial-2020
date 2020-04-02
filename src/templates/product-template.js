f39a69115b2e0e4205f31693010d8675abb2cb30
john-smilga <johnsmilga@gmail.com>
2020-04-02 08:53:50 -0700

    52-template-setup

---
+++ src/templates/product-template.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const productTemplate = () => {
+  return (
+    <div>
+      <h1>hello from product template</h1>
+    </div>
+  )
+}
+
+export default productTemplate
