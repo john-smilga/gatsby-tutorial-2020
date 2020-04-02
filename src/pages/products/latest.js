cc886131c975ba988c1c29cc6ad59d6337bc9d29
john-smilga <johnsmilga@gmail.com>
2020-04-02 08:34:52 -0700

    51-nested-pages

---
+++ src/pages/products/latest.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const latest = () => {
+  return (
+    <div>
+      hello from latest products
+      <p>and the url is /products/latest</p>
+    </div>
+  )
+}
+
+export default latest
